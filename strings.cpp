3687cde9fa65aaa120859638fb1b9406e6f4f465
Shubham Suresh Patil <shubhamsureshpatil@gmail.com>
2019-06-02 23:37:01 -0400

    forth commit

--- strings.cpp
+++ strings.cpp
@@ -2,9 +2,16 @@
 #include <iostream>
 #include <stdint.h>		//for unint64_t
 #include <string.h>
+#include <string>
+
 
 using namespace std;
 
+
+/* avoid pointers in string operations. 
+see https://www.geeksforgeeks.org/basic_string-c_str-function-in-c-stl/ 
+for more details 
+*/
 
 void anagram(){
 	/*string s = [abacbc], one way it can be arranged into a palindrome is aabbcc.
@@ -34,17 +41,53 @@ void string_xor(){
 	char s1[] = "aa";
 }
 
-void only_digits(){
-	/* code to check if the string has only digits
+void only_chars(){
+	/* code to check if the string has only alphabets
 	https://www.quora.com/How-do-Iwrite-a-C++-program-which-checks-that-the-given-string-has-only-letters-and-no-numbers-digits-or-special-symbols
 	*/
-	string s1 = "1234";
-	int i = 0, flag = 0;
 
+	string s1 = "contains numbers and special letters";
+	
+	// remove spaces from the string
+	s1.erase(std::remove_if(s1.begin(), s1.end(), ::isspace), s1.end());	
+
+	int i = 0, flag = 0;
+	while(s1[i] !='\0'){
+		if (!((s1[i] <= 'A' && s1[i] >= 'Z') || (s1[i] >= 'a' && s1[i] <= 'z'))){
+			flag = 1;
+		}
+		i++;
+	}
+	if (flag == 1)
+		cout << "contains numbers and special letters" << std::endl;
+	else
+		cout << "contains only characters" << std::endl;
+}
+
+void Camelcase(){
+	/*
+	code to find the number of letters in Camel case:
+	eg: oneCamelTwo has 3 words in it
+	*/
+	string str = "oneCamelTwo";
+	int count = 1, i = 0;
+	while(str[i] != '\0'){
+		if ((str[i] >= 'A' && str[i] <= 'Z'))
+			count ++;
+		i++;
+	}
+	cout << count << std::endl;
+}
+
+void convert_str_to_int(){
+	std::string str = "12345";
+	int myint = std::stoi(str);
 }
 
 int main(){
-	//anagram();		//Check if string can be rearranged to palindrome
-	only_digits();
+	// anagram();		//Check if string can be rearranged to palindrome
+	// only_chars();		//Check if the string contains only digits
+	// Camelcase();		//Check the number of words in Camelcase
+	convert_str_to_int();	//Convert string int to int 
 	return 0;
 }
